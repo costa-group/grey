0e62cbe1cb33262220946b27e9630989746c8201
alexcere <48130030+alexcere@users.noreply.github.com>
2024-10-04 17:38:58 +0200

    Fix some minor bugs (wrong variables referenced)

--- src/solution_generation/reconstruct_bytecode.py
+++ src/solution_generation/reconstruct_bytecode.py
@@ -4,6 +4,7 @@ Module that contains the methods for reconstructing the bytecode in different fo
 from typing import Dict, Any, List, Optional, Union
 from global_params.types import SMS_T, ASM_bytecode_T, ASM_contract_T
 from parser.cfg import CFG
+from parser.cfg_block import CFGBlock
 
 # Solution ids to EVM assembly
 
@@ -45,7 +46,8 @@ def id_to_asm_bytecode(uf_instrs: Dict[str, Dict[str, Any]], instr_id: str) -> A
     else:
         # The id is the instruction itself (SWAPx, DUPx, ...)
         return asm_from_op_info(instr_id)
-    
+
+
 def id_seq_to_asm_bytecode(uf_instrs: Dict[str, Dict[str, Any]], id_seq: List[str]) -> List[ASM_bytecode_T]:
     """
     Converts a sequence of ids from the greedy algorithm to assembly bytecode
@@ -61,19 +63,22 @@ def asm_from_ids(sms: SMS_T, id_seq: List[str]) -> List[ASM_bytecode_T]:
     return id_seq_to_asm_bytecode(instr_id_to_instr, id_seq)
 
 
-def asm_for_split_instruction_block(block):
-    assert(block.get_jump_type() == "split_instruction_block","[ERROR]: It is not a split_instruction_block")
+def asm_for_split_instruction_block(block: CFGBlock) -> List[ASM_bytecode_T]:
+    """
+    Reconstructs the assembly from a block with a single split instruction
+    """
+    assert block.get_jump_type() == "split_instruction_block", "[ERROR]: It is not a split_instruction_block"
 
     instruction_list = block.get_instructions()
-    assert(len(split_list) == 1, "[ERROR]: Split list should contain only one element")
-    split_ins = split_list[0]
+    assert len(instruction_list) == 1, "[ERROR]: Split list should contain only one element"
+    split_ins = instruction_list[0]
     asm_ins = asm_from_op_info(split_ins.get_op_name().upper())
     asm_subblock = [asm_ins] 
 
     return asm_subblock
 
-def generate_asm_split_blocks(init_block_id, blocks, asm_dicts):
 
+def generate_asm_split_blocks(init_block_id, blocks, asm_dicts):
     asm_block = []
 
     block = blocks[init_block_id]
@@ -84,7 +89,7 @@ def generate_asm_split_blocks(init_block_id, blocks, asm_dicts):
 
         if jump_type == "sub_block":
             asm_subblock = asm_dicts.get(block_id, None)
-            assert(asm_subblock != None, "[ERROR]: subblock should contain an asm block")
+            assert asm_subblock is not None, "[ERROR]: subblock should contain an asm block"
 
         elif jump_type == "split_intruction_block":
             asm_subblock = asm_for_split_instruction_block(block)
@@ -111,7 +116,7 @@ def traverse_cfg(cfg_object, asm_dicts, tags_dict):
     block_list = cfg_object.get_block_list()
     blocks = block_list.get_blocks_dict()
 
-    init_block = blocks[list(blockskeys())[0]]
+    init_block = blocks[list(blocks.keys())[0]]
 
     assert(init_block.get_block_id().find("Block0") != -1)
     
@@ -156,7 +161,7 @@ def traverse_cfg(cfg_object, asm_dicts, tags_dict):
             if falls_to not in blocks or jump_to not in blocks:
                 raise Exception("[ERROR]:...")
 
-            if jumps_to not in visited:
+            if jump_to not in visited:
                 pending_blocks.append(blocks[jump_to])
                 
             if falls_to not in visited:
@@ -174,7 +179,7 @@ def traverse_cfg(cfg_object, asm_dicts, tags_dict):
             if jump_to not in blocks:
                 raise Exception("[ERROR]:...")
 
-            if jumps_to not in visited:
+            if jump_to not in visited:
                 pending_blocks.append(blocks[jump_to])
                 
         elif jump_type == "sub_block" or jump_type == "split_instruction_block":
@@ -185,19 +190,21 @@ def traverse_cfg(cfg_object, asm_dicts, tags_dict):
             try:
                 #It means that the block has been analyzed previously
                 pos = init_pos_dict.index(falls_to)
-                assert(falls_to in visited,"[ERROR]: Falls_to block should be in visited list when generating asm output")
+                assert falls_to in visited, \
+                    "[ERROR]: Falls_to block should be in visited list when generating asm output"
+
                 asm_instructions = asm_instructions[:pos]+asm_block+asm_instructions[pos:]
                 init_pos_dict = init_pos_dict[:pos]+[block_id]*len(asm_block)+init_pos_dict[pos:]
                 
             except ValueError:
-                asm_instructions+=asm_block
-                init_pos_dict+=[block_id]*len(asm_block)
+                asm_instructions += asm_block
+                init_pos_dict += [block_id]*len(asm_block)
 
-                pending_blocks.append(blocks_falls_to)
+                pending_blocks.append(falls_to)
                           
         elif jump_type == "terminal":
-            asm_instructions+=asm_block
-            init_pos_dict+=[block_id]*len(asm_block)
+            asm_instructions += asm_block
+            init_pos_dict += [block_id]*len(asm_block)
 
         else:
             raise Exception("[ERROR]: Unknown jump type when generating asm output")
@@ -235,6 +242,6 @@ def asm_from_cfg(cfg: CFG, asm_dicts: Dict[str, List[ASM_bytecode_T]], tags_dict
 
         json_asm["sourceList"] = [filename]
         
-        json_object[filename +":"+obj_name] = json_asm
+        json_object[filename + ":" +obj_name] = json_asm
         
     return json_object
