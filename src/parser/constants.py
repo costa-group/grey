f41f0e9e0e56b18a05346ca951de93ef06bf21b8
alexcere <48130030+alexcere@users.noreply.github.com>
2024-10-14 17:13:37 +0200

    Add  functionReturn as part of the split instructions

--- src/parser/constants.py
+++ src/parser/constants.py
@@ -1,5 +1,5 @@
 split_block = {'calldatacopy', 'create', 'codecopy', 'call', 'log4', 'gas', 'delegatecall', 'extcodecopy', 'create2',
-               'assignimmutable', 'returndatacopy', 'log2', 'log1', 'log3', 'log0', 'datacopy', 'staticcall'}
-
+               'assignimmutable', 'returndatacopy', 'log2', 'log1', 'log3', 'log0', 'datacopy', 'staticcall',
+               "functionReturn"}
 
 # split_block = {"ASSIGNIMMUTABLE", "GAS", "MEMORYGUARD", "DATACOPY"}
