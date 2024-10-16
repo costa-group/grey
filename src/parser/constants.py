0e822b31a47b51e684e728768f42bca772edca88
alexcere <48130030+alexcere@users.noreply.github.com>
2024-10-16 10:12:14 +0200

    #11 Create functionReturn pseudo-instruction

--- src/parser/constants.py
+++ src/parser/constants.py
@@ -1,5 +1,5 @@
 split_block = {'calldatacopy', 'create', 'codecopy', 'call', 'log4', 'gas', 'delegatecall', 'extcodecopy', 'create2',
                'assignimmutable', 'returndatacopy', 'log2', 'log1', 'log3', 'log0', 'datacopy', 'staticcall',
-               "functionReturn"}
+               "functionReturn", "return"}
 
 # split_block = {"ASSIGNIMMUTABLE", "GAS", "MEMORYGUARD", "DATACOPY"}
