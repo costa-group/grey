8696b9f3739a29bcc5ec29d425c7b3a0efc4a6c1
tutugordillo <tutugordillo@gmail.com>
2025-03-17 08:16:08 +0100

    adding gas consumption

--- scripts/compare_outputs.py
+++ scripts/compare_outputs.py
@@ -14,17 +14,33 @@ def compare_files(json_file1, json_file2):
         print("JSONS have different contract fields")
         return 1
 
+    gas_json1 = 0
+    gas_json2 = 0
     # Drop keys for gas
     for key, json1_answers in json1.items():
         json2_answers = json2[key]
 
-        for answer in [*json1_answers, *json2_answers]:            
-            answer.pop("gasUsed")
-            answer.pop("gasUsedForDeposit")
+        for answer in [*json1_answers]:
+            gas = answer.pop("gasUsed")
+            gas_used = answer.pop("gasUsedForDeposit")
 
+            gas_json1+= int(gas)
+            
+            
+        for answer in [*json2_answers]:
+            gas = answer.pop("gasUsed")
+            gas_used = answer.pop("gasUsedForDeposit")
+
+            gas_json2+=int(gas)
+            
     answer = diff(json1, json2)
     print("FINAL", answer, type(answer))
 
+
+    print("GAS ORIGINAL: "+str(gas_json1))
+    print("GAS OPT: "+str(gas_json2))
+          
+    
     # Empty diff means they are the same
     return 0 if len(answer) == 0 else 1
 
