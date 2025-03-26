9161f3e79966269b7bf7233c275c214da1a7e581
alexcere <48130030+alexcere@users.noreply.github.com>
2025-03-27 00:04:12 +0100

    More sophisticated handle of terminal blocks

--- src/liveness/layout_generation.py
+++ src/liveness/layout_generation.py
@@ -24,7 +24,7 @@ from liveness.liveness_analysis import LivenessAnalysisInfoSSA, construct_analys
     perform_liveness_analysis_from_cfg_info
 from liveness.utils import functions_inputs_from_components
 from liveness.stack_layout_methods import compute_variable_depth, output_stack_layout, unify_stacks_brothers, \
-    compute_block_level, unification_block_dict
+    compute_block_level, unification_block_dict, propagate_output_stack
 
 
 def var_order_repr(block_name: str, var_info: Dict[str, int]):
@@ -158,7 +158,9 @@ class LayoutGeneration:
         if output_stack is None:
             if len(block.successors) == 0:
                 # We just need to place the corresponding elements in the top of the stack
-                output_stack = block.final_stack_elements + input_stack.copy()
+                output_stack = propagate_output_stack(input_stack, block.final_stack_elements,
+                                                      liveness_info.out_state.live_vars, self._variable_order[block_id],
+                                                      block.split_instruction.in_args if block.split_instruction else [])
 
             else:
                 output_stack = output_stack_layout(input_stack, block.final_stack_elements,
