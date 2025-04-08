2c4092225636e7963a87ad388d59cfb359bf2aca
alexcere <alejandrohdezcerezo@gmail.com>
2025-04-08 18:03:32 +0200

    Organize compare_output script better

--- scripts/compare_outputs.py
+++ scripts/compare_outputs.py
@@ -2,42 +2,33 @@ import sys
 import json
 from jsondiff import diff
 
+def information_from_files(json_file):
+    with open(json_file, 'r') as f:
+        json_dict = json.load(f)
+
+    gas_json = 0
+    gas_json_no_deposit = 0
+
+    for key, json_answers in json_dict.items():
+
+        for answer in json_answers:
+            gas = int(answer.pop("gasUsed", 0))
+            gas_deposit = int(answer.pop("gasUsedForDeposit", 0))
+
+            gas_json_no_deposit += gas - gas_deposit
+            gas_json += gas
+        
+        
+    return gas_json, gas_json_no_deposit, json_dict
 
 def compare_files(json_file1, json_file2):
-    with open(json_file1, 'r') as f:
-        json1 = json.load(f)
 
-    with open(json_file2, 'r') as f:
-        json2 = json.load(f)
+    gas_json1, gas_json1_no_deposit, json1 = information_from_files(json_file1)
 
+    gas_json2, gas_json2_no_deposit, json2 = information_from_files(json_file2)    
     if json1.keys() != json2.keys():
         print("JSONS have different contract fields")
         return 1
-
-    gas_json1 = 0
-    gas_json2 = 0
-    # Drop keys for gas
-    gas_json1_no_deposit = 0
-    gas_json2_no_deposit = 0
-
-    for key, json1_answers in json1.items():
-        json2_answers = json2[key]
-
-        for answer in [*json1_answers]:
-            gas = int(answer.pop("gasUsed", 0))
-            gas_deposit = int(answer.pop("gasUsedForDeposit", 0))
-
-            gas_json1_no_deposit += gas - gas_deposit
-            gas_json1+= gas
-
-
-        for answer in [*json2_answers]:
-            gas = int(answer.pop("gasUsed", 0))
-            gas_deposit = int(answer.pop("gasUsedForDeposit", 0))
-
-            gas_json2_no_deposit += gas - gas_deposit
-            gas_json2+=gas
-
 
     answer = diff(json1, json2)
     #print("FINAL", answer, type(answer))
