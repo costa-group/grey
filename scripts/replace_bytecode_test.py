deb9d835cb1ffdf307f53d8ccddb880d8e1a79e8
tutugordillo <tutugordillo@gmail.com>
2025-07-28 15:48:14 +0200

    fixing gas

--- scripts/replace_bytecode_test.py
+++ scripts/replace_bytecode_test.py
@@ -13,8 +13,11 @@ def update_bytecode(json_file_path, contract_name, new_bytecode):
     for c in json_file_path:
  
         json_contract = json_file_path[c]
-
+        print("------")
+        print(c)
+        print(contract_name)
         if contract_name == json_contract["contract"].strip(":"):
+            print("HOLA")
              # Verificar si 'bytecode' está en el JSON
             json_contract["bytecode"] = new_bytecode             
 
@@ -42,32 +45,56 @@ if __name__ == '__main__':
     # Uso del script
     # Sustituye 'ruta/del/archivo.json' con la ruta de tu archivo JSON y 'nuevo_valor' con el valor deseado.
     test_file = sys.argv[1]
-    log_file = sys.argv[2]
-
-    evm_codes = get_evm_code(log_file)
-
-    path_to_test = test_file.split("/")[:-1]
-
-    result_file = "/".join(path_to_test)+"/test_grey"
 
 
-    try:
-        # Leer el archivo JSON
+    if len(sys.argv) == 3: #opt
+        log_file = sys.argv[2]
+        evm_codes = get_evm_code(log_file)
+
+        path_to_test = test_file.split("/")[:-1]
+
+        result_file = "/".join(path_to_test)+"/test_grey"
+
+        try:
+            # Leer el archivo JSON
+            with open(test_file, 'r') as file:
+                data = json.load(file)
+                print(data)
+                for c in evm_codes:
+                    evm = evm_codes[c]
+                    update_bytecode(data, c.strip(), evm.strip())
+
+        except:
+            print("NO TEST: "+test_file)
+        
+    elif len(sys.argv) == 4: #Init test We have to replace the initial code in the test
+        result_file = test_file
+        
         with open(test_file, 'r') as file:
             data = json.load(file)
-
-        for c in evm_codes:
-            evm = evm_codes[c]
-
-            update_bytecode(data, c.strip(), evm.strip())
-
-        # Guardar el archivo JSON actualizado
-
-        with open(result_file, 'w') as file:
-            json.dump(data, file, indent=4)
             
-    except:
-        print("NO TEST: "+test_file)
+            origin_file = sys.argv[2]
+            f = open(origin_file, "r")
+            evm_origin = f.read()
+
+            evm_dict = json.loads(evm_origin)
+            contracts = evm_dict["contracts"]
+
+            for cc in contracts:
+                json_data = contracts[cc]
+                contracts_names = json_data.keys()
+                for c in contracts_names:
+                    print(c)
+                    bytecode = json_data[c.strip()]["evm"]["bytecode"]["object"]
+
+                    update_bytecode(data, c.strip(),bytecode.strip())
+                            
+    else:
+        raise Exception("ERROR IN ARGS")
 
 
-    
+    # Guardar el archivo JSON actualizado
+    with open(result_file, 'w') as file:
+        json.dump(data, file, indent=4)
+        print(data)
+
