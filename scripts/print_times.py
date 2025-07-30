2845dff0d1c7fb42ff94ea407eb7f69f75ec2c50
tutugordillo <tutugordillo@gmail.com>
2025-07-30 22:54:39 +0200

    adding times plots

---
+++ scripts/print_times.py
@@ -0,0 +1,295 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def get_stats(file_name):
+    f = open(file_name, "r")
+    lines = f.readlines()
+
+    all_times_grey_line = list(filter(lambda x: x.find("Times") != -1 and x.find("Total") == -1, lines))[0]
+
+    all_times_aux = all_times_grey_line.split(":")[-1]
+    all_times = list(map(lambda x: float(x.strip()), all_times_aux.split(",")))
+    
+    time_grey_line = list(filter(lambda x: x.find("Total times") != -1, lines))[0]
+    time_grey = time_grey_line.split(":")[-1].strip()
+
+    time_solc_line = list(filter(lambda x: x.find("TIME SOLC") != -1, lines))[0]
+    time_solc = time_solc_line.split(":")[-1].strip()
+
+    blocks_cfg_line = list(filter(lambda x: x.find("Total Blocks CFG") != -1, lines))[0]
+    blocks_cfg = blocks_cfg_line.split(":")[-1].strip()
+
+    ins_cfg_line = list(filter(lambda x: x.find("Total Ins CFG") != -1, lines))[0]
+    ins_cfg = ins_cfg_line.split(":")[-1].strip()
+    
+    return float(time_grey), float(time_solc), int(blocks_cfg), int(ins_cfg), all_times
+
+
+f = "num_instructions.txt"
+
+ff = open(f, "r")
+lines = ff.readlines()
+
+origin = list(filter(lambda x: x.find("ORIGIN")!= -1, lines))
+origin_number = list(map(lambda x: int(x.split(":")[-1].strip()), origin))
+
+f_names = list(map(lambda x: x.split(":")[0].rstrip(" ORIGIN NUM INS"),origin))
+
+total_origin = sum(origin_number)
+
+
+opt = list(filter(lambda x: x.find("OPT")!= -1, lines))
+opt_number = list(map(lambda x: int(x.split(":")[-1].strip()), opt))
+
+total_opt = sum(opt_number)
+
+scalability_cfg = "scalability_cfg.csv"
+f_scal_cfg = open(scalability_cfg, "w")
+
+
+f_scal_cfg.write("Contract name, blocks cfg, ins cfg, Time grey, Time solc,\n")
+
+list_times_grey = []
+list_times_solc = []
+list_ins_cfg = []
+list_blocks_cfg = []
+
+
+time_cfg_generation = []
+time_cfg_parser = []
+time_cfg_preprocess = []
+time_layout = []
+time_greedy = []
+time_asm_generation = []
+time_solc_importer = []
+
+for i in range(len(origin_number)):
+    original = origin_number[i]
+    optimizado = opt_number[i]
+
+    fname = f_names[i]
+    fname_without_ext = fname.rstrip("log")
+
+    print("CHECK: " + str((fname_without_ext+"output", fname_without_ext+"log")))
+   
+
+    time_grey, time_solc, blocks_cfg, ins_cfg, all_times = get_stats(fname_without_ext+"log")
+
+    time_cfg_generation.append(all_times[0])
+    time_cfg_parser.append(all_times[1])
+    time_cfg_preprocess.append(all_times[2])
+    time_layout.append(all_times[3])
+    time_greedy.append(all_times[4])
+    time_asm_generation.append(all_times[5])
+    time_solc_importer.append(all_times[6])
+    
+    list_times_grey.append(time_grey)
+    list_times_solc.append(time_solc)
+    list_ins_cfg.append(ins_cfg)
+    list_blocks_cfg.append(blocks_cfg)
+    
+    f_scal_cfg.write(",".join([fname_without_ext[:-1],str(blocks_cfg), str(ins_cfg),str(time_grey),str(time_solc)])+"\n")
+
+# Plots with instructions
+    
+paired = sorted(zip(list_ins_cfg, list_times_grey))
+ins_cfg_sorted, times_grey_sorted = zip(*paired)
+
+ins_cfg_sorted, times_grey_sorted = list(ins_cfg_sorted), list(times_grey_sorted)
+
+paired_solc = sorted(zip(list_ins_cfg, list_times_solc))
+ins_cfg_sorted, times_solc_sorted = zip(*paired_solc)
+
+ins_cfg_sorted, times_solc_sorted = list(ins_cfg_sorted), list(times_solc_sorted)
+
+index_list = range(len(ins_cfg_sorted))
+
+plt.figure()
+# Crear DataFrame
+df = pd.DataFrame({"x": index_list, "y": times_grey_sorted})
+
+# Dibujar scatter
+sns.scatterplot(data=df, x="x", y="y")
+
+plt.xlabel("Contracts ordered by number of instructions")
+plt.ylabel("Time (s)")
+plt.title("Grey")
+
+
+plt.savefig("figs/scatter_plot_grey.png")
+#plt.show()
+
+plt.figure()
+# Crear DataFrame
+df_solc = pd.DataFrame({"x": index_list, "y": times_solc_sorted})
+
+# Dibujar scatter
+sns.scatterplot(data=df_solc, x="x", y="y")
+
+plt.xlabel("Contracts ordered by number of instructions")
+plt.ylabel("Time (s)")
+plt.title("Solc")
+
+
+plt.savefig("figs/scatter_plot_solc.png")
+#plt.show()
+
+plt.figure()
+# Crear DataFrame
+df = pd.DataFrame({"x": ins_cfg_sorted, "y": times_grey_sorted})
+
+# Dibujar scatter
+sns.scatterplot(data=df, x="x", y="y")
+
+plt.xlabel("Num Instructions")
+plt.ylabel("Time (s)")
+plt.title("Grey")
+
+
+plt.savefig("figs/scatter_plot_grey_ins.png")
+#plt.show()
+
+plt.figure()
+# Crear DataFrame
+df_solc = pd.DataFrame({"x": ins_cfg_sorted, "y": times_solc_sorted})
+
+# Dibujar scatter
+sns.scatterplot(data=df_solc, x="x", y="y")
+
+plt.xlabel("Num Instructions")
+plt.ylabel("Time (s)")
+plt.title("Solc")
+
+
+plt.savefig("figs/scatter_plot_solc_ins.png")
+#plt.show()
+
+
+#Plots with blocks
+    
+paired = sorted(zip(list_blocks_cfg, list_times_grey))
+blocks_cfg_sorted, times_grey_sorted = zip(*paired)
+
+blocks_cfg_sorted, times_grey_sorted = list(blocks_cfg_sorted), list(times_grey_sorted)
+
+paired_blocks_solc = sorted(zip(list_blocks_cfg, list_times_solc))
+blocks_cfg_sorted, times_solc_sorted = zip(*paired_solc)
+
+blocks_cfg_sorted, times_solc_sorted = list(blocks_cfg_sorted), list(times_solc_sorted)
+
+index_list = range(len(blocks_cfg_sorted))
+
+plt.figure()
+# Crear DataFrame
+df = pd.DataFrame({"x": index_list, "y": times_grey_sorted})
+
+# Dibujar scatter
+sns.scatterplot(data=df, x="x", y="y")
+
+plt.xlabel("Contracts ordered by number of blocks")
+plt.ylabel("Time (s)")
+plt.title("Grey")
+
+
+plt.savefig("figs/scatter_plot_grey_blocks_relative.png")
+#plt.show()
+
+plt.figure()
+# Crear DataFrame
+df_solc = pd.DataFrame({"x": index_list, "y": times_solc_sorted})
+
+# Dibujar scatter
+sns.scatterplot(data=df_solc, x="x", y="y")
+
+plt.xlabel("Contracts ordered by number of blocks")
+plt.ylabel("Time (s)")
+plt.title("Solc")
+
+
+plt.savefig("figs/scatter_plot_solc_blocks_relative.png")
+#plt.show()
+
+plt.figure()
+# Crear DataFrame
+df = pd.DataFrame({"x": blocks_cfg_sorted, "y": times_grey_sorted})
+
+# Dibujar scatter
+sns.scatterplot(data=df, x="x", y="y")
+
+plt.xlabel("Num Blocks")
+plt.ylabel("Time (s)")
+plt.title("Grey")
+
+
+plt.savefig("figs/scatter_plot_grey_blocks.png")
+#plt.show()
+
+plt.figure()
+# Crear DataFrame
+df_solc = pd.DataFrame({"x": blocks_cfg_sorted, "y": times_solc_sorted})
+
+# Dibujar scatter
+sns.scatterplot(data=df_solc, x="x", y="y")
+
+plt.xlabel("Num Blocks")
+plt.ylabel("Time (s)")
+plt.title("Solc")
+
+
+plt.savefig("figs/scatter_plot_solc_blocks.png")
+#plt.show()
+
+
+#Times by phases
+
+paired = sorted(zip(list_ins_cfg, time_cfg_generation))
+ins_cfg_sorted, time_cfg_generation_sorted = zip(*paired)
+
+ins_cfg_sorted, times_cfg_generation_sorted = list(ins_cfg_sorted), list(time_cfg_generation_sorted)
+
+paired = sorted(zip(list_ins_cfg, time_cfg_parser))
+ins_cfg_sorted, time_cfg_parser_sorted = zip(*paired)
+
+ins_cfg_sorted, times_cfg_parser_sorted = list(ins_cfg_sorted), list(time_cfg_parser_sorted)
+
+paired = sorted(zip(list_ins_cfg, time_cfg_preprocess))
+ins_cfg_sorted, time_cfg_preprocess_sorted = zip(*paired)
+
+ins_cfg_sorted, times_cfg_preprocess_sorted = list(ins_cfg_sorted), list(time_cfg_preprocess_sorted)
+
+paired = sorted(zip(list_ins_cfg, time_layout))
+ins_cfg_sorted, time_layout_sorted = zip(*paired)
+
+ins_cfg_sorted, times_layout_sorted = list(ins_cfg_sorted), list(time_layout_sorted)
+
+paired = sorted(zip(list_ins_cfg, time_greedy))
+ins_cfg_sorted, time_greedy_sorted = zip(*paired)
+
+ins_cfg_sorted, times_greedy_sorted = list(ins_cfg_sorted), list(time_greedy_sorted)
+
+paired = sorted(zip(list_ins_cfg, time_asm_generation))
+ins_cfg_sorted, time_asm_generation_sorted = zip(*paired)
+
+ins_cfg_sorted, times_asm_generation_sorted = list(ins_cfg_sorted), list(time_asm_generation_sorted)
+
+paired = sorted(zip(list_ins_cfg, time_solc_importer))
+ins_cfg_sorted, time_solc_importer_sorted = zip(*paired)
+
+ins_cfg_sorted, times_solc_importer_sorted = list(ins_cfg_sorted), list(time_solc_importer_sorted)
+
+ys = [time_cfg_generation_sorted, time_cfg_parser_sorted, time_cfg_preprocess_sorted, time_layout_sorted, time_greedy_sorted, time_asm_generation_sorted, time_solc_importer_sorted]
+labels = ['CFG Generation', 'CFG Parser', 'CFG Preprocess', 'Layout Generation', 'Greedy', 'ASM Generation', 'solc Importer']
+
+plt.figure()
+
+for y, label in zip(ys, labels):
+    plt.plot(ins_cfg_sorted, y, label=label)
+
+# Personalizar gráfico
+plt.xlabel('Instructions in CFG')
+plt.ylabel('Time (s)')
+plt.title('Time of different phases')
+plt.legend()
+plt.grid(True)
+plt.show()
