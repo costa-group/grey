e73c3a2467252992ad0b0c2b29f90130686a5e20
tutugordillo <tutugordillo@gmail.com>
2025-09-24 10:24:55 +0200

    working on stats

--- scripts/get_stats.sh
+++ scripts/get_stats.sh
@@ -5,7 +5,18 @@ RES_FILE=$1
 grep -F "NUM INS:" $RES_FILE &> num_instructions.txt
 grep -F "GAS:" $RES_FILE &> num_gas.txt
 grep -F "NUM BYTES:" $RES_FILE &> num_bytes.txt
+grep "NUM INS" $RES_FILE &> all_num_instructions.txt
+
+echo "HOLA"
+
+grep Times test/*/*.log | cut -d':' -f3- &> salida.csv
+
+
+python times.py
+python print_times.py
+python plot_solc_solx_grey.py
 
 python sum_instructions.py
 python sum_gas.py
 python sum_bytes.py
+
