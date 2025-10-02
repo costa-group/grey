dfb85db8c2bc5f5e694ba5998a6a3aa882a9be83
alexcere <48130030+alexcere@users.noreply.github.com>
2025-10-02 17:49:54 +0200

    Instruction deps with all the instructions + no split from opcodes

--- src/parser/constants.py
+++ src/parser/constants.py
@@ -1,5 +1,5 @@
-split_block = {'calldatacopy', 'create', 'codecopy', 'call', 'log4', 'gas', 'delegatecall', 'extcodecopy', 'create2',
-               'assignimmutable', 'returndatacopy', 'log2', 'log1', 'log3', 'log0', 'datacopy', 'staticcall','tstore','tload','mcopy'}
+split_block = {} # {'calldatacopy', 'create', 'codecopy', 'call', 'log4', 'gas', 'delegatecall', 'extcodecopy', 'create2',
+                 #  'assignimmutable', 'returndatacopy', 'log2', 'log1', 'log3', 'log0', 'datacopy', 'staticcall','tstore','tload','mcopy'}
 
 terminal_ops = {"functionReturn", "functionReturn", "return", "revert", "stop", "selfdestruct"}
 
