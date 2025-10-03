ba85c106e7cec499db2c41d9a42ca5c586e7de3d
alexcere <48130030+alexcere@users.noreply.github.com>
2025-10-03 07:50:54 +0200

    Mark new ops with storage value set to true to avoid removing them from the sfs

--- src/parser/constants.py
+++ src/parser/constants.py
@@ -3,7 +3,12 @@ split_block = {} # {'calldatacopy', 'create', 'codecopy', 'call', 'log4', 'gas',
 
 terminal_ops = {"functionReturn", "functionReturn", "return", "revert", "stop", "selfdestruct"}
 
+memory_storage_instructions = {'calldatacopy', 'create', 'codecopy', 'call', 'log4', 'gas', 'delegatecall',
+                               'extcodecopy', 'create2', 'assignimmutable', 'returndatacopy', 'log2', 'log1', 'log3',
+                               'log0', 'datacopy', 'staticcall', 'tstore', 'mstore', 'mcopy', 'mstore8', 'sstore8'}
+
 # split_block = {"ASSIGNIMMUTABLE", "GAS", "MEMORYGUARD", "DATACOPY"}
+
 
 def add_verbatim_to_split_block(verbatim_inst):
     split_block.add(verbatim_inst)
