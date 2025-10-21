03d2920abc21b471841a74ca5e483a1d28217bd4
alexcere <48130030+alexcere@users.noreply.github.com>
2025-10-21 12:52:13 +0200

    #29 Colour assignment considering available of variable size

--- src/reparation/colour_assignment.py
+++ src/reparation/colour_assignment.py
@@ -13,7 +13,6 @@ class ColourAssignment:
     """
     def __init__(self):
         self._total_colors: int = 0
-        self._used_colors: int = 0
 
         # We determine which variable is associated to a colour
         self._var2color: Dict[block_id_T, int] = dict()
@@ -26,27 +25,26 @@ class ColourAssignment:
         """
         Chooses an available colour, adding a new one if there are not enough
         """
+        i = 0
+        found_colour = False
+        while not found_colour and i < len(available):
+            if available[i]:
+                available[i] = False
+                found_colour = True
+                self._var2color[v] = i
+            else:
+                i += 1
+
         # If all colours are used, we need to increase the number of colours
-        if self._total_colors == self._used_colors:
-            self._total_colors += 1
-            self._used_colors += 1
+        if not found_colour:
+            new_color = len(available)
             available.append(False)
-            self._var2color[v] = self._total_colors - 1
-            return self._total_colors - 1
-        else:
-            i = 0
-            found_colour = False
-            while not found_colour and i < self._total_colors:
-                if available[i]:
-                    available[i] = False
-                    found_colour = True
-                    self._used_colors += 1
-                    self._var2color[v] = i
-                else:
-                    i += 1
-            if not found_colour:
-                raise ValueError("There must exist a colour that has not been assigned so far")
-            return i
+            self._var2color[v] = new_color
+            # Update the number of colours (might not be initialized
+            # for other cases)
+            self._total_colors = max(self._total_colors, new_color + 1)
+            return new_color
+        return i
 
     def pick_specific_colour(self, v: var_id_T, available: List[bool], color: int):
         """
@@ -54,24 +52,13 @@ class ColourAssignment:
         """
         assert available[color], f"Picked color {color} for variable {v} is not available"
         available[color] = False
-        self._used_colors += 1
         self._var2color[v] = color
 
     def has_variable(self, v: var_id_T):
         return v in self._var2color
 
     def release_colour(self, v: var_id_T, available: List[bool]):
-        self._used_colors -= 1
         available[self._var2color[v]] = True
-
-    def colour_set(self, var_set: Set[var_id_T]):
-        """
-        Colours all the elements in the set with a new color
-        """
-        for var_ in var_set:
-            self._var2color[var_] = self._total_colors
-        self._total_colors += 1
-        self._used_colors += 1
 
     def uncolour_var(self, var_: var_id_T):
         """
