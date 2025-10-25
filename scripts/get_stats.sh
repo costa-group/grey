36fb28bcf8324d1dd74637b12c7f96c9fa8418aa
tutugordillo <tutugordillo@gmail.com>
2025-10-25 23:49:01 +0200

    adding own gas

--- scripts/get_stats.sh
+++ scripts/get_stats.sh
@@ -5,6 +5,7 @@ RES_FILE=$1
 grep -F "NUM INS:" $RES_FILE &> num_instructions.txt
 grep -F "GAS:" $RES_FILE &> num_gas.txt
 grep -F "NUM BYTES:" $RES_FILE &> num_bytes.txt
+grep -F "OWN COSTAG:" $RES_FILE &> num_own_gas.txt
 grep "NUM INS" $RES_FILE &> all_num_instructions.txt
 
 echo "HOLA"
@@ -20,5 +21,6 @@ python plot_solc_solx_grey.py
 python sum_instructions.py
 python sum_gas.py
 python sum_bytes.py
+python sum_own_gas.py
 
 python count_solx.py
