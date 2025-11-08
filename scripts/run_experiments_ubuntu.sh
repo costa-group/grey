b1910f10f4ef2cc7e4f679373d43d6c97bc74c6f
tutugordillo <tutugordillo@gmail.com>
2025-11-08 17:04:20 +0100

    working on experiments

--- scripts/run_experiments_ubuntu.sh
+++ scripts/run_experiments_ubuntu.sh
@@ -6,6 +6,9 @@ DIRECTORIO_BASE=/home/pablo/Repositorios/ethereum/grey/scripts/test
 GREY_PATH=/home/pablo/Repositorios/ethereum/grey/src/grey_main.py
 SOLC_PATH=/home/pablo/Repositorios/ethereum/grey/solc-latest
 TESTRUNNER_PATH=/home/pablo/Repositorios/ethereum/solidity/build/test/tools/testrunner
+SOLX_PATH=/hoome/pablo/Repositorios/ethereum/solx/solx-macosx-profiling
+TEST_SOLX_PATH=/home/pablo/Repositorios/ethereum/grey/scripts/test_solx
+
 EVMONE_LIB=/home/pablo/Repositorios/ethereum/evmone/build/lib/libevmone.so
 
 # Comprobar si el directorio existe
@@ -19,13 +22,20 @@ fi
 
 # start=$(date +%s.%N)
 
+# find "$DIRECTORIO_BASE" -type f -name "*standard_input.json" |  grep '/externalContract[^/]*/' | while read -r yul_file; do
+
 find "$DIRECTORIO_BASE" -type f -name "*standard_input.json" | while read -r yul_file; do
 
+    
     # Obtener el directorio y el nombre base del archivo
 
     yul_dir=$(dirname "$yul_file")
     yul_base=$(basename "$yul_file" _standard_input.json)
 
+    test_dir_name=$(basename "$yul_dir")
+
+    solx_test_file="$test_dir_name/${yul_base}_standard_input.json"
+    
     echo "Procesando archivo: $yul_file"
 
     pushd $yul_dir
@@ -39,8 +49,20 @@ find "$DIRECTORIO_BASE" -type f -name "*standard_input.json" | while read -r yul
     
     echo "$SOLC_PATH $yul_file --standard-json &> $yul_dir/$yul_base.output"
 
+    #SOLX EXECUTION
+    start_solx=$(date +%s.%N)
+    $SOLX_PATH --standard-json "$TEST_SOLX_PATH/$solx_test_file" &> "$yul_dir/$yul_base.solx_output"
+    end_solx=$(date +%s.%N)
+    echo "$start_solx"
+    echo "$end_solx"
+    elapsed_solx=$(echo "$end_solx - $start_solx" | bc)
+    echo "TIME SOLX $yul_file : $elapsed_solx"
+    
+    echo "$SOLX_PATH --standard-json $TEST_SOLX_PATH/$solx_test_file   &> $yul_dir/$yul_base.solx_output"
+    
+
     start=$(date +%s.%N)
-    python3 $GREY_PATH -s "$yul_file" -g -v -if standard-json -solc $SOLC_PATH -o "/tmp/$yul_base" &> "$yul_dir/$yul_base.log"
+    python3 $GREY_PATH -s "$yul_file" -g -j -if standard-json -solc $SOLC_PATH -o "/tmp/$yul_base" &> "$yul_dir/$yul_base.log"
     end=$(date +%s.%N)
     popd
     elapsed=$(echo "$end - $start" | bc)
@@ -58,20 +80,33 @@ find "$DIRECTORIO_BASE" -type f -name "*standard_input.json" | while read -r yul
     
         python3 replace_bytecode_test.py "$yul_dir/test" "$yul_dir/$yul_base.log"
 
+        python3 replace_bytecode_test.py "$yul_dir/test" "$yul_dir/$yul_base.output" init
+        
         echo "python3 replace_bytecode_test.py $yul_dir/test $yul_dir/$yul_base.log"
 
+        echo "python3 replace_bytecode_test.py $yul_dir/test $yul_dir/$yul_base.output init"
+
+        
         $TESTRUNNER_PATH  $EVMONE_LIB $yul_dir/test $yul_dir/resultOriginal.json
 
         $TESTRUNNER_PATH  $EVMONE_LIB $yul_dir/test_grey $yul_dir/resultGrey.json
 
         # python3 compare_outputs.py $yul_dir/resultOriginal.json $yul_dir/resultGrey.json $yul_file
         python3 compare_outputs.py $yul_dir/resultOriginal.json $yul_dir/test $yul_dir/resultGrey.json $yul_dir/test_grey $yul_file
+        echo "python3 compare_outputs.py $yul_dir/resultOriginal.json $yul_dir/test $yul_dir/resultGrey.json $yul_dir/test_grey $yul_file"
         RES=$?
         # if diff $yul_dir/resultOriginal.json $yul_dir/resultGrey.json > /dev/null; then
         if [ $RES -eq 0 ]; then
             echo "[RES]: Test passed."
             echo "python3 count_num_ins.py $yul_dir/$yul_base.output $yul_dir/$yul_base.log"
-            python3 count_num_ins.py "$yul_dir/$yul_base.output" "$yul_dir/$yul_base.log"
+            python3 count_num_ins.py "$yul_dir/$yul_base.output" "$yul_dir/$yul_base.log" "$yul_dir/$yul_base.solx_output"
+
+
+            echo "python3 prepare_hevm.py $yul_dir/$yul_base.output $yul_dir/$yul_base.log"
+            python3 prepare_hevm.py "$yul_dir/$yul_base.output" "$yul_dir/$yul_base.log"
+
+            echo "python3 compare_solx.py $yul_dir/$yul_base.log $yul_dir/$yul_base.solx_output $yul_dir/intermediate.json"
+            python3 compare_solx.py "$yul_dir/$yul_base.log" "$yul_dir/$yul_base.solx_output" "$yul_dir/intermediate.json"
             
         else
             echo "[RES]: Test failed."
@@ -83,7 +118,6 @@ find "$DIRECTORIO_BASE" -type f -name "*standard_input.json" | while read -r yul
     fi
     
     echo "*************************************"
-
     
 done
 
