65f3d9c26bf0bbfd5545ab7ea675b0542f17248b
tutugordillo <tutugordillo@gmail.com>
2025-11-09 12:11:04 +0100

    minor

--- scripts/get_stats.sh
+++ scripts/get_stats.sh
@@ -2,6 +2,7 @@
 
 RES_FILE=$1
 
+
 grep -F "NUM INS:" $RES_FILE &> num_instructions.txt
 grep -F "GAS:" $RES_FILE &> num_gas.txt
 grep -F "NUM BYTES:" $RES_FILE &> num_bytes.txt
@@ -10,11 +11,13 @@ grep "NUM INS" $RES_FILE &> all_num_instructions.txt
 
 
 grep Times test/*/*.log | cut -d':' -f3- &> salida.csv
+grep TIME $RES_FILE &> salida-times.txt
+
 
 grep "SOLXRES" $RES_FILE &> solx_res.txt
 
 python times.py
-python print_times.py
+python print_times.py num_instructions.txt
 python plot_solc_solx_grey.py
 
 python sum_instructions.py num_instructions.txt
